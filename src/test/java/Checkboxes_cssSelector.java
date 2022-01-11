d5085014876383b74d112f3e9519cab5e293fdbc
corinasel <97466033+corinasel@users.noreply.github.com>
2022-01-11 20:19:01 +0200

    load css Selector and xpath test

---
+++ src/test/java/Checkboxes_cssSelector.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Checkboxes_cssSelector {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","C:/Users/Corina/Desktop/manual testing/Selenium testing/chromedriver.exe"); // set the path of chromedriver
+        WebDriver driverChrome=new ChromeDriver();
+        driverChrome.get("https://formy-project.herokuapp.com/checkbox");
+        Thread.sleep(3000);
+        driverChrome.findElement(By.id("checkbox-1")).click();
+        Thread.sleep(3000);
+
+        WebElement checkbox2= driverChrome.findElement(By.cssSelector("#checkbox-2"));
+        checkbox2.click();
+        Thread.sleep(3000);
+        driverChrome.findElement(By.xpath("//*[@id=\"checkbox-3\"]")).click();
+        Thread.sleep(3000);
+        driverChrome.quit();
+
+    }
+}
